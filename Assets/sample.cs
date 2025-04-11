d252cbdc223c2cdc5545df03fa6406121fb2ed09
notrllyxyion <mikelreuelramos69@gmail.com>
2025-04-11 13:27:01 +0800

    yyguuugu

--- Assets/sample.cs
+++ Assets/sample.cs
@@ -4,19 +4,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sample : MonoBehaviour
-{ 
+{
 
     private void Start()
     {
-        static void MyMethod()
-        {
-            Debug.Log("Ilove you");
-        }
-        static void Main(string[] args)
-        {
-            MyMethod();
-            MyMethod();
-        }
+
     }
 }
 
